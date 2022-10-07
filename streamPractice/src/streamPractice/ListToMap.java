3b0cfa1b37dc50e4fb67a1a582bb0bf2c6e27168
hodob <hodob76@gmail.com>
2022-10-07 17:54:34 +0900

    중간저장

---
+++ streamPractice/src/streamPractice/ListToMap.java
@@ -0,0 +1,28 @@
+package streamPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		List<Person> personList = new ArrayList<>();
+		personList.add(new Person("짱구", 23, "010-1234-1234"));
+		personList.add(new Person("유리", 24, "010-1234-1234"));
+		personList.add(new Person("철수", 12, "010-1234-4652"));
+		personList.add(new Person("맹구", 23, null));
+		
+		// Function.identity는 t -> t, 항상 입력된 인자(자신)를 반환합니다.
+		Map<String, Person> personMap = personList.stream()
+				.collect(Collectors.toMap(Person::getName, Function.identity()));
+		System.out.println(personMap);
+	}
+
+}
